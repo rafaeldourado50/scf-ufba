b93b74fcf2c83d45a93abf2041d9cace571373ce
Rafael Dourado <rafaeldourado10@gmail.com>
2018-02-16 23:43:21 -0300

    Ajustando menu da aplicação.

--- resources/views/admin/menu.blade.php
+++ resources/views/admin/menu.blade.php
@@ -1,4 +1,3 @@
 <li><a href="{{ url('/home') }}">Home</a></li>
 <li><a href="{{ url('/plano') }}">Planos</a></li>
 <li><a href="{{ url('/import') }}">Importar</a></li>
-<li><a href="{{ url('/chamada') }}">Chamadas</a></li>
